554e95dda6bbd9b46e9f0c742a68f2e1ca00d172
Ariel <arielox.ag@gmail.com>
2022-02-23 13:48:08 -0300

    api listo auth y user routes

--- api/index.js
+++ api/index.js
@@ -3,11 +3,21 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
-// const userRoute = require('./routes/users');
-// const postRoute = require('./routes/posts');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 // const categoryRoute = require('./routes/categories');
 // const multer = require('multer');
 // const path = require('path');
+
+/* 
+
+
+
+29.56
+
+
+
+*/
 
 dotenv.config();
 app.use(express.json());
@@ -24,8 +34,8 @@ app.use(express.json());
 //    .catch(err => console.log(err));
 mongoose
    .connect(process.env.MONGO_URL)
-   .then(console.log('Connected to MongoDB.....🍑'));
-//    .catch(err => console.log(err));;
+   .then(console.log('Connected to MongoDB.....🍑'))
+   .catch(err => console.log(err));
 
 // const storage = multer.diskStorage({
 //    destination: (req, file, cb) => {
@@ -42,8 +52,8 @@ mongoose
 // });
 
 app.use('/api/auth', authRoute);
-// app.use('/api/users', userRoute);
-// app.use('/api/posts', postRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 // app.use('/api/categories', categoryRoute);
 
 app.listen('5000', () => {
