7739e135ab423606d9580ad5b503b0b6e23a9430
Ariel <arielox.ag@gmail.com>
2022-02-22 22:28:47 -0300

    inicial

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import Topbar from './components/topbar/Topbar';
+import Homepage from './pages/homepage/Homepage';
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+// import Settings from "./pages/settings/Settings";
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+/* 
+
+1.10.11
+
+*/
+function App() {
+   return (
+      <>
+         <Topbar />
+         <Write />
+      </>
+   );
+}
+
+export default App;
